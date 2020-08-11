9a20f7068d28db6f9991895cec27770d502335f8
Souma <souma.hitech@gmail.com>
2020-08-11 22:52:28 +0530

    add home login and register route.

--- app.js
+++ app.js
@@ -13,8 +13,22 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-//TODO
+app.get("/",function(req,res){
+    res.render("home");
+});
 
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+
+
+
+//open 3000 port for access
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
